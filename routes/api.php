e9e613af6ae99d3996300e6b1df44c2e28f958c4
hani <athiyyahaniifa@gmail.com>
2025-05-23 14:00:27 +0700

    23Mei(1)

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\APIGuruController;
+use App\Http\Controllers\APIIndustriController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiresource("guru", APIGuruController::class);  //mengarahkan ke APIGuruController
+Route::apiresource("industri", APIIndustriController::class);  //mengarahkan ke APIGuruController
