10ca9ff3987e0170fdb19310c50dfbf9e63e3f4f
hani <athiyyahaniifa@gmail.com>
2025-05-30 17:50:19 +0700

    30Mei(2)

--- app/Filament/Resources/PklResource.php
+++ app/Filament/Resources/PklResource.php
@@ -71,12 +71,12 @@ class PklResource extends Resource
             ->filters([])
             ->actions([
                 Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
             ]);
+            // ->bulkActions([
+            //     Tables\Actions\BulkActionGroup::make([
+            //         Tables\Actions\DeleteBulkAction::make(),
+            //     ]),
+            // ]);
     }
 
 
